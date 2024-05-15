4dfb3583fda300efb90b3d90eeea9a010d25d950
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-15 07:16:33 -0500

    Problemas solucionados

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Blog_UDLA_PZ_JS_DM.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
