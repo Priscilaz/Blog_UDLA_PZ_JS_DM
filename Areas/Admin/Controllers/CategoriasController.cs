43c2962bfe4a30a4403e1016d2dbde8d86b1a903
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-21 22:41:07 -0500

    Creación de la vista de lista de categorías

---
+++ Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,34 @@
+﻿
+using BlogUDLA.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_UDLA_PZ_JS_DM.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        public CategoriasController(IContenedorTrabajo contenedorTrabajo)
+        {
+           _contenedorTrabajo= contenedorTrabajo;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region Llamadas a la APi
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Categoria.GetAll()});
+        } 
+
+
+
+        #endregion
+    }
+}
