0fe645a3a8679d2d4106d3b110cc992034d611bc
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-24 10:35:08 -0500

    Cambios en la forma de mostrar la lista de categorias

--- wwwroot/js/categoria.js
+++ wwwroot/js/categoria.js
@@ -14,22 +14,22 @@ function cargarDatatable() {
         },
         "columns": [
             { "data": "id", "width": "5%" },
-            { "data": "nombre", "width": "50%" },
-            { "data": "orden", "width": "20%" },
+            { "data": "nombre", "width": "40%" },
+            { "data": "orden", "width": "10%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                                <a href="/Admin/Categorias/Edit/${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
-                                <i class="far fa-edit"> Editar
+                                <a href="/Admin/Categorias/Edit/${data}" class="btn btn-success text-white" style="cursor:pointer; width:140px;">
+                                <i class="far fa-edit"></i> Editar
                                 </a>
                                 &nbsp;
-                                <a onclick=Delete("/Admin/Categorias/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
-                                <i class="far fa-trash-alt"> Borrar
+                                <a onclick=Delete("/Admin/Categorias/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:140px;">
+                                <i class="far fa-trash-alt"></i> Borrar
                                 </a>
                             </div>
                             `;
-                }, "width": "30%"
+                }, "width": "40%"
             }
         ],
         "language": {
