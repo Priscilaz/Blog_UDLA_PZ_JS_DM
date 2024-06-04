e16d6f0d509e5b3cb99de0f0f41d2a1f4a00e58c
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-06-04 10:49:52 -0500

    Añado algunas funcionalidades de la pestaña Articulos

---
+++ Areas/Admin/Controllers/ArticulosController.cs
@@ -0,0 +1,29 @@
+﻿using BlogUDLA.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_UDLA_PZ_JS_DM.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class ArticulosController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Llamadas a la APi
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Articulo.GetAll(includeProperties: "Categoria") });
+        }
+        #endregion
+    }
+}
