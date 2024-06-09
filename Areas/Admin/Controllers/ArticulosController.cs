47ae4065c9159ca29e090ee7464393d0c2109184
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-06-09 10:34:32 -0500

    Funcionalidad editar articulos aniadida

--- Areas/Admin/Controllers/ArticulosController.cs
+++ Areas/Admin/Controllers/ArticulosController.cs
@@ -9,9 +9,13 @@ namespace Blog_UDLA_PZ_JS_DM.Areas.Admin.Controllers
     public class ArticulosController : Controller
     {
         private readonly IContenedorTrabajo _contenedorTrabajo;
-        public ArticulosController(IContenedorTrabajo contenedorTrabajo)
+        private readonly IWebHostEnvironment _hostingEnvironment;
+       
+
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo, IWebHostEnvironment hostingEnvironment)
         {
             _contenedorTrabajo = contenedorTrabajo;
+            _hostingEnvironment = hostingEnvironment;
         }
         [HttpGet]
         public IActionResult Index()
@@ -31,12 +35,148 @@ namespace Blog_UDLA_PZ_JS_DM.Areas.Admin.Controllers
             return View(artiVM);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ArticuloVM artiVM)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string rutaPrincipal = _hostingEnvironment.WebRootPath;
+                var archivos = HttpContext.Request.Form.Files;
+                if (artiVM.Articulo.Id == 0 && archivos.Count() > 0)
+                {
+                    //Nuevo asticulo
+                    string nombreArchivo = Guid.NewGuid().ToString();
+                    var subidas = Path.Combine(rutaPrincipal, @"imagenes\articulos");
+                    var extension = Path.GetExtension(archivos[0].FileName);
+
+                    using (var fileStreams = new FileStream(Path.Combine(subidas, nombreArchivo + extension), FileMode.Create))
+                    {
+                        archivos[0].CopyTo(fileStreams);
+
+                    }
+                    artiVM.Articulo.UrlImagen = @"\imagenes\articulos\" + nombreArchivo + extension;
+                    artiVM.Articulo.FechaCreacion = DateTime.Now.ToString();
+
+                    _contenedorTrabajo.Articulo.Add(artiVM.Articulo);
+                    _contenedorTrabajo.Save();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError("Imagen", "Debes seleccionar una imagen");
+                }
+            }   
+            artiVM.ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias();
+            return View(artiVM);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            ArticuloVM artiVM = new ArticuloVM()
+            {
+                Articulo = new BlogUDLA.Models.Articulo(),
+                ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias()
+
+            };
+            if (id != null)
+            {
+                artiVM.Articulo = _contenedorTrabajo.Articulo.Get(id.GetValueOrDefault());
+            }
+            return View(artiVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ArticuloVM artiVM)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string rutaPrincipal = _hostingEnvironment.WebRootPath;
+                var archivos = HttpContext.Request.Form.Files;
+
+                var articuloDesdeBd = _contenedorTrabajo.Articulo.Get(artiVM.Articulo.Id);
+
+
+
+
+                if (archivos.Count() > 0)
+                {
+                    //Nueva imagen para el  articulo
+                    string nombreArchivo = Guid.NewGuid().ToString();
+                    var subidas = Path.Combine(rutaPrincipal, @"imagenes\articulos");
+                    var extension = Path.GetExtension(archivos[0].FileName);
+                    var nuevaExtension = Path.GetExtension(archivos[0].FileName);
+
+                    var rutaImagen = Path.Combine(rutaPrincipal, articuloDesdeBd.UrlImagen.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(rutaImagen))
+                    {
+                        System.IO.File.Delete(rutaImagen);
+                    }
+                    //Nuevamente subimos el archivo
+                    using (var fileStreams = new FileStream(Path.Combine(subidas, nombreArchivo + extension), FileMode.Create))
+                    {
+                        archivos[0].CopyTo(fileStreams);
+
+                    }
+                    artiVM.Articulo.UrlImagen = @"\imagenes\articulos\" + nombreArchivo + extension;
+                    artiVM.Articulo.FechaCreacion = DateTime.Now.ToString();
+
+                    _contenedorTrabajo.Articulo.Update(artiVM.Articulo);
+                    _contenedorTrabajo.Save();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    //Cuando la imagen ya existe y se conserva
+                    artiVM.Articulo.UrlImagen = articuloDesdeBd.UrlImagen;
+                }
+                _contenedorTrabajo.Articulo.Update(artiVM.Articulo);
+                _contenedorTrabajo.Save();
+
+                return RedirectToAction(nameof(Index));
+            }
+            artiVM.ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias();
+            return View(artiVM);
+        }
+
+
+
         #region Llamadas a la APi
         [HttpGet]
         public IActionResult GetAll()
         {
             return Json(new { data = _contenedorTrabajo.Articulo.GetAll(includeProperties: "Categoria") });
         }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var articuloDesdeDb = _contenedorTrabajo.Articulo.Get(id);
+            string rutaDirectorioPrincipal = _hostingEnvironment.WebRootPath;
+            var rutaImagen = Path.Combine(rutaDirectorioPrincipal, articuloDesdeDb.UrlImagen.TrimStart('\\'));
+
+            if (System.IO.File.Exists(rutaImagen))
+            {
+                System.IO.File.Delete(rutaImagen);
+            }
+
+            if (articuloDesdeDb == null)
+            {
+                return Json(new { success = false, message = "Error borrando artículo" });
+            }
+            _contenedorTrabajo.Articulo.Remove(articuloDesdeDb);
+            _contenedorTrabajo.Save();
+            return Json(new { success = true, message = "Artículo eliminado" });
+        }
+
+
         #endregion
     }
 }
--- wwwroot/js/articulos.js
+++ wwwroot/js/articulos.js
@@ -16,6 +16,12 @@ function cargarDatatable() {
             { "data": "id", "width": "5%" },
             { "data": "nombre", "width": "20%" },
             { "data": "categoria.nombre", "width": "15%" },
+            {
+                "data": "urlImagen",
+                "render": function (imagen) {
+                    return `<img src="../${imagen}" width="120px">`
+                }
+            },
             { "data": "fechaCreacion", "width": "30%" },
 
             {
